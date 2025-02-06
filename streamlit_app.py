347d20212d0ed60e5154f98a86d8e0999a752d34
test point <167181348+AORNG@users.noreply.github.com>
2025-02-06 08:45:15 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,68 @@
 import streamlit as st
+import pandas as pd
 import sqlite3
+import hashlib
 
-def create_users_table():
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)')
+#パスワードの保存
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+
+#パスワードのハッシュ化
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+    if make_hashes(password) == hashed_text:
+        return hashed_text
+    return False
+
+#ユーザー登録
+def create_user():
+    c.execute("CREATE TABLE IF NOT EXISTS usertable(username TEXT,password TEXT)")
+
+#ユーザー追加
+def add_user(username,password):
+    c.execute("INSERT INTO usertable(user,password) VALUES (?,?)",(username,password))
     conn.commit()
-    conn.close()
 
-def add_user(username, password):
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-    c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
-    conn.commit()
-    conn.close()
-
-def check_user(username, password):
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+#ログイン
+def login_user(user,password):
+    c.execute("SELECT * FROM usertable WHERE username =? AND password = ?",(username,password))
     data = c.fetchall()
-    conn.close()
-    return len(data) != 0
+    return data
 
+#画面作成
 def main():
-    st.sidebar.title("Authentication Demo")
-    menu = ["ホーム", "ログイン", "サインアップ"]
-    choice = st.sidebar.radio("メニュー", menu)
+    st.title("教員用")
+
+    menu = ["ホーム","ログイン","サインアップ"]
+    choice = st.sidebar.selectbox("メニュー",menu)
 
     if choice == "ホーム":
-        st.subheader("ホーム")
-        # You can display whatever you want on the home page
+        st.subheader("ホーム画面")
 
     elif choice == "ログイン":
-        st.subheader("ログイン Section")
-        username = st.sidebar.text_input("User Name")
-        password = st.sidebar.text_input("Password", type='password')
+        username = st.sidebar.text_input("ユーザー名を入力")
+        password = st.sidebar.text_input("パスワードを入力",type="password")
+        if st.sidebar.checkbox("ログイン"):
+            create_user()
+            hashed_pswd = make_hashes(password)
 
-        if st.sidebar.button("ログイン"):
-            if check_user(username, password):
-                st.success("Logged In as {}".format(username))
-                task = st.selectbox("Task", ["Add Post", "Analytics", "Profiles"])
-                if task == "Add Post":
-                    st.subheader("Add Your Post")
-                elif task == "Analytics":
-                    st.subheader("Analytics")
-                elif task == "Profiles":
-                    st.subheader("User Profiles")
+            result = login_user(username,check_hashes(password,hashed_pswd))
+            if result:
+                st.success("{}さんでログインしました".format(username))
+            
             else:
-                st.warning("Incorrect Username/Password")
-
+                st.warning("ユーザー名かパスワードが間違っています")
     elif choice == "サインアップ":
-        st.subheader("Create New Account")
-        new_user = st.text_input("Username")
-        new_password = st.text_input("Password", type='password')
+        st.subheader("新しいアカウントを作成")
+        new_user = st.text_input("ユーザー名を入力")
+        new_password = st.text_input("パスワードを入力",type="password")
 
         if st.button("サインアップ"):
-            add_user(new_user, new_password)
-            st.success("You have successfully created an account.")
-            st.info("Go to Login Menu to login")
-
-if __name__ == "__main__":
-    create_users_table()
+            create_user()
+            add_user(new_user,make_hashes(new_password))
+            st.success("アカウントの作成しました")
+            st.info("ログイン画面からログインしてください")
+if __name__ == "__name__":
     main()
