bcb36324a06642d62d832840581645753d3bb62b
test point <167181348+AORNG@users.noreply.github.com>
2025-03-05 07:32:59 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def add_user(username,password):
     conn.commit()
 
 #ログイン
-def login_user(user,password):
+def login_user(username,password):
     c.execute("SELECT * FROM usertable WHERE username =? AND password = ?",(username,password))
     data = c.fetchall()
     return data
