d1c1da032e8487e158252bc8777efa84a7a5e8cd
test point <167181348+AORNG@users.noreply.github.com>
2025-03-18 07:53:04 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,18 +12,16 @@ st.set_page_config(
    layout="wide",
 )
 
-# Excelファイルのアップロード
-uploaded_file = st.file_uploader("Excelファイルをアップロード", type=["xlsx"])
-
-if uploaded_file is not None:
-    try:
-        df = pd.read_excel(uploaded_file, sheet_name="1年1組", header=0, usecols="A:F")
-        df = df.dropna()  # 空白データがある行を除外
-        st.dataframe(df)
-    except Exception as e:
-        st.error(f"Excel ファイルの読み込みに失敗しました: {e}")
-else:
-    st.info("ファイルをアップロードしてください。")
+# Excelデータ読み込み
+# ここでファイルパスを修正しました
+try:
+    df = pd.read_excel("/mnt/network/74回生/個人フォルダ/1年次/2組/1221/日課表.xlsx", sheet_name="1年1組", header=0, usecols="A:F")
+    df = df.dropna()  # 空白データがある行を除外
+    st.dataframe(df)
+except FileNotFoundError:
+    st.error("ファイルが見つかりません。正しいパスを確認してください。")
+except Exception as e:
+    st.error(f"Excel ファイルの読み込みに失敗しました: {e}")
 
 # データベース接続の作成
 conn = sqlite3.connect("database.db")
@@ -86,4 +84,36 @@ def main():
         check_button = st.button("ボタン")
         if check_button:
             send_post_request(
-                'https://prod-01.japaneast.logic.azure.com:443/workflows/38f7b8c8d476411d8d4351e0638c6750/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=
+                'https://prod-01.japaneast.logic.azure.com:443/workflows/38f7b8c8d476411d8d4351e0638c6750/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DQl_g5amg0IRCFIs1lRiIBvicQ1Z9JI9i7uNgWKKu2g',
+                check
+            )
+
+    elif choice == "ログイン":
+        username = st.sidebar.text_input("ユーザー名を入力")
+        password = st.sidebar.text_input("パスワードを入力", type="password")
+        if st.sidebar.checkbox("ログイン"):
+            create_user_table()
+            if login_user(username, password):
+                st.success(f"{username}さんでログインしました")
+            else:
+                st.warning("ユーザー名かパスワードが間違っています")
+
+    elif choice == "サインアップ":
+        st.subheader("新しいアカウントを作成")
+        new_user = st.text_input("ユーザー名を入力")
+        new_password = st.text_input("パスワードを入力", type="password")
+
+        if st.button("サインアップ"):
+            create_user_table()
+            if user_exists(new_user):
+                st.warning("このユーザー名はすでに使われています。別の名前を試してください。")
+            else:
+                add_user(new_user, make_hashes(new_password))
+                st.success("アカウントの作成が完了しました")
+                st.info("ログイン画面からログインしてください")
+
+    # データベース接続を閉じる
+    conn.close()
+
+if __name__ == "__main__":
+    main()
