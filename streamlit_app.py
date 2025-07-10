3e70827b2885b5c7f9c721dd220d45fe0fbffcc5
test point <167181348+AORNG@users.noreply.github.com>
2025-07-10 08:15:17 +0000

    q

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
+import streamlit as st
 import feedparser
 
+st.title("BBC News RSS Feed")
+
 feed = feedparser.parse("https://feeds.bbci.co.uk/news/rss.xml")
+
 for entry in feed.entries[:5]:
-    print(entry.title)
-    print(entry.link)
+    st.subheader(entry.title)
+    st.write(entry.link)
